741535fa680c405a2e028413909286d1a4880c74
NISHI Satoshi <protophasma7cm@gmail.com>
2024-01-04 18:53:36 +0900

    Add files via upload

---
+++ root.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+mkdir root-mv
+
+mv bin root-mv
+mv boot root-mv
+mv dev root-mv
+mv etc root-mv
+mv home root-mv
+mv lib root-mv
+mv lib32 root-mv
+mv lib64 root-mv
+mv libx32 root-mv
+mv media root-mv
+mv mnt root-mv
+mv opt root-mv
+mv proc root-mv
+mv root root-mv
+mv run root-mv
+mv sbin root-mv
+mv srv root-mv
+mv sys root-mv
+mv usr root-mv
+mv var root-mv
